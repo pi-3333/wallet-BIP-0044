42fb5135739498a0886d267ee6494b13fdf6a8d2
gasnani1 <72911406+gasnani1@users.noreply.github.com>
2021-05-31 22:03:41 +1000

    bitcoin litecoin testnet

---
+++ src/networks/bitcoin.js
@@ -0,0 +1,28 @@
+import { Insight } from '@oipwg/insight-explorer'
+import { networks } from '@oipwg/bitcoinjs-lib'
+import bip44constants from 'bip44-constants'
+import config from './config'
+
+const bitcoinFeePerKb = 100000
+
+const n = networks.bitcoin
+
+n.slip44 = bip44constants.BTC
+
+module.exports = {
+  name: 'bitcoin',
+  displayName: 'Bitcoin',
+  ticker: 'BTC',
+  satPerCoin: 1e8,
+  feePerKb: bitcoinFeePerKb,
+  feePerByte: bitcoinFeePerKb / 1000,
+  maxFeePerByte: 100,
+  minFee: 0,
+  dust: 546,
+
+  txVersion: 1,
+
+  explorer: new Insight(config.defaultApiUrls.bitcoin),
+
+  network: n
+}
---
+++ src/networks/litecoin.js
@@ -0,0 +1,36 @@
+import { Insight } from '@oipwg/insight-explorer'
+import coininfo from 'coininfo'
+import bip44constants from 'bip44-constants'
+import config from './config'
+
+const litecoinFeePerKb = 100000
+
+const n = coininfo.litecoin.main.toBitcoinJS()
+
+module.exports = {
+  name: 'litecoin',
+  displayName: 'Litecoin',
+  ticker: 'LTC',
+  satPerCoin: 1e8,
+  feePerKb: litecoinFeePerKb,
+  feePerByte: litecoinFeePerKb / 1000,
+  maxFeePerByte: 100,
+  minFee: 0,
+  dust: 54600,
+
+  txVersion: 1,
+
+  explorer: new Insight(config.defaultApiUrls.litecoin),
+
+  network: {
+    bip32: {
+      public: n.bip32.public,
+      private: n.bip32.private
+    },
+    slip44: bip44constants.LTC,
+    messagePrefix: '\u0018Litecoin Signed Message:\n',
+    pubKeyHash: n.pubKeyHash,
+    scriptHash: n.scriptHash,
+    wif: n.wif
+  }
+}
